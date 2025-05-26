ff03f121db61284bfb7dc87621b7397b18e026c3
emse-p4a-gwu <john.helveston@gmail.com>
2025-05-26 15:02:45 -0400

    rebuild site

--- render.R
+++ render.R
@@ -1,17 +1,18 @@
-library(quarto)
 library(renderthis)
 
 # Render slides 
-parts <- c('storytelling', 'visualizing-information')
+parts <- c(
+    'getting-started', 'conditional-control', 'data-handling', 'reactivity'
+)
 for (part in parts) {
     path_rmd <- file.path('parts', part, 'index.Rmd')
     path_html <- file.path('parts', part, 'index.html')
     path_pdf <- file.path('parts', part, paste0(part, '.pdf'))
     to_html(from = path_rmd, to = path_html)
-    to_pdf(from = path_html, to = path_pdf, partial_slides = TRUE)
+    to_pdf(from = path_html, to = path_pdf)
     # Compress the PDF to reduce size
     tools::compactPDF(path_pdf, gs_quality = 'ebook')
 }
 
 # Render site
-quarto_render()
+quarto::quarto_render()
